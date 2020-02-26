48a9f9d3cf09ee6debd28a8f154c83621ce4367c
jakpra <jp1724@georgetown.edu>
2020-02-25 21:51:58 -0500

    forked & modified

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+# with open('requirements.txt') as f:
+#     requirements = f.read().splitlines()
+
+setup(
+    name='Transformers',
+    version='0.0.0',
+    # install_requires=requirements,
+    packages=['Transformers'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
